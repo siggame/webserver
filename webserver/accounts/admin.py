256adfd9d9add31127ec70003f646e1e3b7fadc5
Michael Wisely <michaelwisely@gmail.com>
2013-02-10 12:59:46 -0600

    Added profile info to User admin interface

---
+++ webserver/accounts/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from models import UserProfile
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class FancyUserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
+
+admin.site.unregister(User)
+admin.site.register(User, FancyUserAdmin)
