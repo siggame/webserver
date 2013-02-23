cd07dfe9fa50497df83f557f832509b253413f84
Michael Wisely <michaelwisely@gmail.com>
2013-02-23 15:45:34 -0600

    Created a template tag to prettify repo names

---
+++ webserver/codemanagement/templatetags/code_tags.py
@@ -0,0 +1,12 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+import re
+
+register = template.Library()
+
+
+@register.filter
+@stringfilter
+def clean_repo_name(value):
+    return re.sub(r'__\d+\.git', '.git', value)
