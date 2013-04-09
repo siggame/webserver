ac3f6a8480f90cfe1a79154cf22f65e52854a697
Michael Wisely <michaelwisely@gmail.com>
2013-04-09 17:30:02 -0500

    Allow TeamSubmission submitter field to be null

--- webserver/codemanagement/models.py
+++ webserver/codemanagement/models.py
@@ -86,7 +86,7 @@ class TeamSubmission(models.Model):
     name = models.CharField(max_length=50,
                             validators=[tag_validator],
                             help_text="Choose a name for this submission")
-    submitter = models.ForeignKey(User)
+    submitter = models.ForeignKey(User, null=True)
     tag_time = models.DateTimeField(auto_now_add=True)
     submission_time = models.DateTimeField(auto_now_add=True)
 
