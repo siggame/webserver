2ca3f28b4423fc8ecd19591a039b7a5c814ab25b
Michael Wisely <michaelwisely@gmail.com>
2013-04-11 16:53:54 -0500

    Make dulwich check the tag.

--- webserver/codemanagement/validators.py
+++ webserver/codemanagement/validators.py
@@ -1,7 +1,20 @@
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from dulwich.repo import check_ref_format
+
+import re
 
 sha1_validator = RegexValidator(regex="^[a-f0-9]{40}$",
                                 message="Must be valid sha1 sum")
-tag_validator = RegexValidator(regex="^[A-Za-z][\w\-\.]+[A-Za-z]$",
-                               message="Must be letters and numbers" +
-                               " separated by dashes, dots, or underscores")
+tag_regex = re.compile(r'^[A-Za-z][\w\-\.]+[A-Za-z]$')
+
+
+def tag_validator(value):
+    if not tag_regex.match(value):
+        msg = "Must be letters and numbers separated "
+        msg += "by dashes, dots, or underscores"
+        raise ValidationError(msg)
+    if not check_ref_format('refs/tags/' + value):
+        msg = "Invalid tag. Tags must adhere to ref formats defined here: "
+        msg += "https://www.kernel.org/pub/software/scm/git/docs/git-check-ref-format.html"
+        raise ValidationError(msg)
