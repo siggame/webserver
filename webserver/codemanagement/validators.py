d7af7e06c21979873d32cd987713b320dc10cc78
Michael Wisely <michaelwisely@gmail.com>
2013-04-11 16:12:51 -0500

    Update tag regex

--- webserver/codemanagement/validators.py
+++ webserver/codemanagement/validators.py
@@ -2,6 +2,6 @@ from django.core.validators import RegexValidator
 
 sha1_validator = RegexValidator(regex="^[a-f0-9]{40}$",
                                 message="Must be valid sha1 sum")
-tag_validator = RegexValidator(regex="^[\w\-\.]+$",
+tag_validator = RegexValidator(regex="^[A-Za-z][\w\-\.]+[A-Za-z]$",
                                message="Must be letters and numbers" +
                                " separated by dashes, dots, or underscores")
