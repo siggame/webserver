f37d4f560564e31ca453bd85b06b6341c549a7df
Michael Wisely <michaelwisely@gmail.com>
2013-04-11 00:42:49 -0500

    Add api_url data to game extra_data

--- webserver/hermes/tasks.py
+++ webserver/hermes/tasks.py
@@ -45,7 +45,7 @@ def populate_score(api, game=None):
     # Set extra data for the glog.
     extra = score.data
     up = {'output_url': api['output_url'],
-                  'version': api['version']}
+          'version': api['version']}
     try:
         extra.update(up)
     except AttributeError:
@@ -89,7 +89,8 @@ def fetch_games(arena_api_url, competition_slug,
 
             # Try to load or create a game object using get_or_create
             status = game['status']
-            extra = {'gamelog_url': game["gamelog_url"]}
+            extra = {'gamelog_url': game["gamelog_url"],
+                     'api_url': arena_api_url}
             defaults = {
                 'start_time': datetime.today() if status in HAS_STARTED else None,
                 'end_time': datetime.today() if status in HAS_FINISHED else None,
@@ -156,7 +157,8 @@ def update_games(arena_api_url, competition_slug,
                     game.end_time = datetime.today() if status in HAS_FINISHED else None
                 if status in HAS_FINISHED:
                     data = game.data
-                    data.update({'gamelog_url': obj["gamelog_url"]})
+                    data.update({'gamelog_url': obj["gamelog_url"],
+                                 'api_url': arena_api_url})
                     game.extra_data = json.dumps(data)
                 game.status = status
                 game.save()
