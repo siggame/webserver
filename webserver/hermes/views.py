106657fae3a8c7bd815826f6d7aaf66ea6287138
Michael Wisely <michaelwisely@gmail.com>
2013-11-08 13:53:26 -0600

    Update the game stats view

--- webserver/hermes/views.py
+++ webserver/hermes/views.py
@@ -1,11 +1,18 @@
-from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 
-from competition.views.mixins import CompetitionViewMixin, RequireRunningMixin
+from competition.views.game_views import GameView
+from .models import TeamStats
 
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-class GameStatsView(CompetitionViewMixin, RequireRunningMixin, TemplateView):
+class GameStatsView(GameView, DetailView):
     template_name = 'hermes/game_stats.html'
+    context_object_name = 'stats'
+
+    def get_object(self):
+        self.team = self.get_team()
+        stats, _ = TeamStats.objects.get_or_create(team=self.team)
+        return stats
