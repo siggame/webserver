a3d28d1d95565901db69ab4e5b8a6ecfaadc84a6
Michael Wisely <michaelwisely@gmail.com>
2013-11-08 13:50:58 -0600

    Create a database model for storing game statistics
    It's just a text field for JSON data, but it should make it a lot
    easier to store stats. It can be cached later if necessary.

--- webserver/hermes/models.py
+++ webserver/hermes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+from django.conf import settings
 
-# Create your models here.
+from competition.models import Team
+
+import json
+
+
+class TeamStats(models.Model):
+    team = models.OneToOneField(Team)
+    data_field = models.TextField(null=True, default="null")
+
+
+    @property
+    def data(self):
+        return json.loads(self.data_field)
+
+    @data.setter
+    def data(self, value):
+        self.data_field = json.dumps(value)
