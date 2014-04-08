9a914eb0c0c19eee875e98152998b8e752022832
jswv9@mst.edu <jswv9@mst.edu>
2014-04-08 16:30:36 -0500

    Rework homepage to show open / running competitions for unauthenticated users

--- webserver/home/views.py
+++ webserver/home/views.py
@@ -9,6 +9,9 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
 
+        context["open_competitions"] = Competition.objects.filter(is_open=True)
+        context["running_competitions"] = Competition.objects.filter(is_running=True)
+
         if not self.request.user.is_anonymous():
             my_competitions = Competition.objects.user_registered(self.request.user)
             context["registered_competitions"] = my_competitions.exclude(is_running=False, is_open=False)
