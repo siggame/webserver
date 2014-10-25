cdfdfd7418f33cc38aa7db3e42e0050d4189ab77
jswv9@mst.edu <jswv9@mst.edu>
2014-10-25 13:36:40 -0500

    Update active templatetag to accept more complex strings

--- webserver/utility/templatetags/active_tags.py
+++ webserver/utility/templatetags/active_tags.py
@@ -1,14 +1,16 @@
 import re
 from django import template
 from django.conf import settings
-
+from django.template import Context, Template
 
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def active(context, pattern):
-    print pattern
     request = context['request']
-    if re.search(pattern, request.path):
+    template = Template(pattern)
+    context = Context(context)
+
+    if re.search(template.render(context), request.path):
         return 'active'
     return ''
