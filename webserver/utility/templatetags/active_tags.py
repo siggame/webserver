512eff989c5c11c092def01c28520cb892f1cbfe
Michael Wisely <michaelwisely@gmail.com>
2014-10-28 22:12:40 -0500

    Add another active template tag
    One template tag is for passing URL regex's, the other accepts the name
    of a URL from a URLconf

--- webserver/utility/templatetags/active_tags.py
+++ webserver/utility/templatetags/active_tags.py
@@ -1,16 +1,34 @@
 import re
 from django import template
-from django.conf import settings
 from django.template import Context, Template
+from django.core.urlresolvers import resolve, Resolver404
+
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
-def active(context, pattern):
+def active_re(context, pattern, *classes):
+    classes = classes or ['active']
     request = context['request']
     template = Template(pattern)
     context = Context(context)
 
     if re.search(template.render(context), request.path):
-        return 'active'
+        return ' '.join(classes)
+    return ''
+
+
+@register.simple_tag(takes_context=True)
+def active(context, url_name, *classes):
+    classes = classes or ['active']
+    request = context['request']
+
+    try:
+        resolved = resolve(request.get_full_path())
+    except Resolver404:
+        return ""
+
+    if resolved.url_name == url_name:
+        return ' '.join(classes)
     return ''
