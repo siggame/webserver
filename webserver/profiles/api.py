a933b31beb8ac77170a3a31bdb014cf6a1a50b3d
Michael Wisely <michaelwisely@gmail.com>
2014-11-03 20:13:46 -0600

    Limit info provided by the API

--- webserver/profiles/api.py
+++ webserver/profiles/api.py
@@ -1,13 +1,29 @@
+from django.contrib.auth.models import User
+
 from rest_framework import generics, serializers
 from rest_framework.permissions import IsAuthenticated
 
 from .models import UserProfile
 
 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'fullname')
+
+    fullname = serializers.SerializerMethodField('get_fullname')
+
+    def get_fullname(self, obj):
+        return obj.get_full_name()
+
+
 class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
 
     class Meta:
         model = UserProfile
+        fields = ('user', 'url')
 
     url = serializers.SerializerMethodField('get_url')
 
