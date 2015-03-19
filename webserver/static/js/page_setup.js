63e86140b20a373649d676ad29a46a833f76f9e0
Michael Wisely <michaelwisely@gmail.com>
2015-03-19 17:51:09 -0500

    Remove iframe resizing code

--- webserver/static/js/page_setup.js
+++ webserver/static/js/page_setup.js
@@ -1,18 +1,7 @@
 $(function() {
-
   $('[data-toggle="tooltip"]').tooltip();
 
   $('.navbar-toggle').click(function() {
     $('#sidebar-wrapper').toggleClass('toggled');
   });
-
-  resize_home_iframes = function() {
-    var boxes = $('.embed-responsive.home-content');
-    var content_height = $(window).outerHeight() - $(".footer").outerHeight() - boxes.offset().top;
-    $('.embed-responsive.home-content').css('padding-bottom', content_height);
-  }
-
-  $(window).resize(resize_home_iframes);
-  resize_home_iframes();
-
 });
