a63617edf201e316e6d01d80ebfcddd3f05368e5
Michael Wisely <michaelwisely@gmail.com>
2015-03-19 16:42:19 -0500

    Create API endpoints for fetching feeds
    An unfortunate work-around to address our HTTPS/HTTP iframe woes.

--- webserver/home/urls.py
+++ webserver/home/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, url
 
-from views import HomePageView
+from views import HomePageView, BlogFeedAPIView, StatusFeedAPIView
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$',
-        HomePageView.as_view(),
-        name='home'),
+    url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^api/blog-feed/$', BlogFeedAPIView.as_view(), name='blog-feed'),
+    url(r'^api/status-feed/$', StatusFeedAPIView.as_view(), name='status-feed'),
 )
